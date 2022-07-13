1adfa48a5951ae606fbaa928c639ad65709fa963
Arthur Castro <56102054+ArthurC04@users.noreply.github.com>
2022-07-13 14:00:31 -0300

    Delete aboutme.js

--- src/events/interactions/buttons/aboutme.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    config: {
-        name: 'aboutmebt',
-        customId: 'aboutme',
-    },
-    run: async (client, interaction) => {
-        interaction.reply(`Meu nome é ...., sou desenvolvido com ...`);
-    },
-}
