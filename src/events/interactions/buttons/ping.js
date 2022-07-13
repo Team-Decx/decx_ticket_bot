483d879b36160ff0a7d7ac5b1e74587ff1a3f3df
Arthur Castro <56102054+ArthurC04@users.noreply.github.com>
2022-07-13 14:00:26 -0300

    Delete ping.js

--- src/events/interactions/buttons/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    config: {
-        name: 'pingbt',
-        customId: 'ping',
-    },
-    run: async (client, interaction) => {
-        interaction.reply(`Meu ping é de estimados ${client.ws.ping} ms.`)
-    },
-}
