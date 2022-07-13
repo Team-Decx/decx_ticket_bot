a34286bce572e22428aa8655097621cbad6b4efe
Arthur Castro <56102054+ArthurC04@users.noreply.github.com>
2022-07-13 14:00:44 -0300

    Delete guildMemberAdd.js

--- src/events/guild/guildMemberAdd.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client, member) => {
-    const guild = client.guilds.cache.get('938611908234326017');
-    const role = guild.roles.cache.get('944037645590331442');
-    await member.roles.add(role.id);
-    const welcomeChannel = client.channels.cache.find(channel => channel.id === '944038163146485771');
-    return await welcomeChannel.send(`${member.user.username} entrou no servidor!`);
-};
