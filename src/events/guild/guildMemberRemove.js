4dd38cf83a6f619371b85e5a1f0d880c7972c824
Arthur Castro <56102054+ArthurC04@users.noreply.github.com>
2022-07-13 14:00:49 -0300

    Delete guildMemberRemove.js

--- src/events/guild/guildMemberRemove.js
+++
@@ -1,4 +0,0 @@
-module.exports = async (client, member) => {
-    const leftChannel = client.channels.cache.find(channel => channel.id === '944038174596956261');
-    return await leftChannel.send(`${member.user.username} saiu do servidor!`);
-};
